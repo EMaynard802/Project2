61f2adf2e81de92cc3ea7b3ecd0b1e2cd3cb3370
Ouro <ourosalimabdul@gmail.com>
2020-03-10 19:38:26 -0400

    AO

---
+++ models/Order.js
@@ -0,0 +1,26 @@
+module.exports = function(sequelize, DataTypes) 
+{
+    var Order = sequelize.define("Order", 
+    {
+      order_quatity:
+      {
+        type: DataTypes.INTEGER,
+        isInt: true,
+        min: 1,
+        notEmpty: true,
+        allowNull: false,
+        unique: false
+
+      }
+    });
+    Order.associate = function(models) {
+        // We're saying that an Order should belong to a User
+        // An Order can't be created without an User due to the foreign key constraint
+        Order.belongsTo(models.User, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+    return Order;
+};
